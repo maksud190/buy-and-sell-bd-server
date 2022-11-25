ade735cffe4c35d83a3bd24c480b188e5ff26303
maksud190 <maknep3rdoct@gmail.com>
2022-11-25 10:01:30 +0600

    mongodb added and api created

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
 const port = process.env.PORT || 5000;
 const app = express();
 
@@ -7,6 +10,35 @@ const app = express();
 // MiddleWares 
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xngqds2.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run(){
+
+    try{
+        const categoryNameAndItemCollection = client.db('buyAndSellBd').collection('categoryNameAndItem')
+
+        app.get('/categories', async(req, res)=> {
+            const query = {}
+            const categories = await categoryNameAndItemCollection.find(query).toArray();
+            res.send(categories);
+        })
+    }
+
+    finally{
+
+    }
+
+}
+
+run().catch(console.log);
+
+
 
 app.get('/', async(req, res)=> {
     res.send('Buy and Sell server is Running');
